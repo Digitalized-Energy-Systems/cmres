ef6a8d79acaafe1cc67c2eafe3bfa111111add01
Rico Schrage <rico.schrage@uni-oldenburg.de>
2023-03-07 11:03:01 +0100

    Adding Stubs for non-finished stuff

---
+++ secmes/omef/solver/ea.py
@@ -0,0 +1,2 @@
+class EASolver:
+    pass
---
+++ secmes/omef/solver/eval.py
@@ -0,0 +1,14 @@
+class OMEFEvaluator:
+    pass
+
+
+def is_productive():
+    pass
+
+
+def is_broken():
+    pass
+
+
+def calc_relative_violation():
+    pass
